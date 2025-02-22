8ae2ccc29a13bc7e979af7bf7553e2710ba21ca8
Ignacia22 <mfernandezpolanco@gmail.com>
2025-02-22 17:03:38 -0300

    cambio

--- portafolio1/src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-      <div className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl font-bold mb-4">404 - Página no encontrada</h1>
-        <p>Lo sentimos, la página que buscas no existe.</p>
-      </div>
-    )
-  }
