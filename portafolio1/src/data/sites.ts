abf873465249599643b551a124cea404e5aafec9
Ignacia22 <mfernandezpolanco@gmail.com>
2025-03-13 15:23:26 -0300

    se realizaron mejoras

---
+++ portafolio1/src/data/sites.ts
@@ -0,0 +1,36 @@
+export type Sites2 = {
+    id: number;
+    color: string;
+    title: string;
+    desc: string;
+    img: string;
+    link: string;
+} 
+
+
+export const Sites: Sites2[] = [
+    {
+        id: 1,
+        color: "bg-gradient-to-r from-red-300 to-blue-300",
+        title: "Hola",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minima esse fuga, sequi atque animi culpa eveniet delectus repellat possimus a est nostrum ipsam dicta voluptatibus consequuntur explicabo earum repellendus.",
+        img: "https://res.cloudinary.com/dosbg5xdd/image/upload/v1741885481/Rectangle_10_yc2r19.jpg",
+        link: "https://example.com",
+    },
+    {
+        id: 2,
+        color: "bg-gradient-to-r from-blue-300 to-violet-300",
+        title: "Club deportivo",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minima esse fuga, sequi atque animi culpa eveniet delectus repellat possimus a est nostrum ipsam dicta voluptatibus consequuntur explicabo earum repellendus.",
+        img: "https://res.cloudinary.com/dosbg5xdd/image/upload/v1741885499/WhatsApp_Image_2025-02-13_at_2.55.02_PM_jr5lul.jpg",
+        link: "https://example.com",
+    },
+    {
+        id: 3,
+        color: "bg-gradient-to-r from-violet-300 to-purple-300",
+        title: "futbol",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde minima esse fuga, sequi atque animi culpa eveniet delectus repellat possimus a est nostrum ipsam dicta voluptatibus consequuntur explicabo earum repellendus.",
+        img: "https://res.cloudinary.com/dosbg5xdd/image/upload/v1741885655/pexels-long-ba-mui-1130557677-30379858_f7kfuo.jpg",
+        link: "https://example.com",
+    },
+] 
