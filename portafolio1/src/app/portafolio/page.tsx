9182fcbd5fbc24e8b4f937f0611228e70336ffdc
Ignacia22 <mfernandezpolanco@gmail.com>
2025-03-13 20:42:16 -0300

    Agregue carrousel de proyectos

--- portafolio1/src/app/portafolio/page.tsx
+++ portafolio1/src/app/portafolio/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 import { Sites } from "@/data/sites";
@@ -13,7 +12,7 @@ const PortafolioPage = () => {
 
     const {scrollYProgress} = useScroll({target: ref})
 
-    const x = useTransform(scrollYProgress, [0,1], ["0%", "100%"])
+    const x = useTransform(scrollYProgress, [0,1], ["0%", "-70%"])
 
     return (
         <motion.div 
@@ -26,7 +25,9 @@ const PortafolioPage = () => {
                 <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center text-blue-950">
                     My Works
                 </div>
-                <div className="sticky top-0 flex h-screen gap-4 items-center">
+                <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+                  <motion.div style={{x}} className="flex">
+                    <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300"/>
                 {Sites.map(Site => {
                  // Determinar el color basado en el ID
                  let gradientClass = "";
@@ -41,8 +42,8 @@ const PortafolioPage = () => {
                    >
                      {/* Resto del contenido */}
                      <div className="flex flex-col gap-8 text-white">
-                       <h1>{Site.title}</h1>
-                       <div className="relative w-full h-64">
+                       <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{Site.title}</h1>
+                       <div className="relative w-80 h-64 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                          <Image
                            src={Site.img}
                            alt=""
@@ -50,14 +51,42 @@ const PortafolioPage = () => {
                            className="object-cover"
                          />
                        </div>
-                       <p>{Site.desc}</p>
-                       <Link href={Site.link}><button>See demo</button></Link>
+                       <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">{Site.desc}</p>
+                       <Link 
+                       href={Site.link}
+                       className="flex justify-end"
+                       >
+                        <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                          See demo
+                        </button>
+                        </Link>
                      </div>
                    </div>
                  );
                })}
+               </motion.div>
                 </div>
             </div>
+               <div className="w-screen h-screen flex flex-col gap-16 items-center  justify-center text-center">
+                <h1 className="text-8xl">Tienes un proyecto?</h1>
+                <div className="relative">
+                <motion.svg animate={{rotate: 360}} transition={{duration: 8, ease:"linear", repeat: Infinity}} viewBox="0 0 300 300" className="w-64 h-64 md:w-[500px] md:h-[500px]" xmlns="http://www.w3.org/2000/svg">
+                  <defs>
+                    <path id="circlePath" d="M150 90 a 60,60 0 1,0 0 120 a 60,60 0 1,0 0 -120" stroke="#000000" strokeWidth="1" fill="none"/>
+                  </defs>
+                  <text fill="#000000">
+                    <textPath xlinkHref="#circlePath" className="text-xl">
+                      Front-end Developer and Web Designer
+                    </textPath>
+                  </text>
+                </motion.svg>
+                  <Link 
+                  href="/contact" 
+                  className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
+                  >SIGUEME
+                  </Link>
+                </div>
+               </div>
         </motion.div>
     )
 }
